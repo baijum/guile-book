55e5632c0ebbc5c3be591e2440c44f7b19acabdc
Baiju Muthukadan <baiju.m.mail@gmail.com>
2017-05-30 11:02:21 +0530

    remove release

--- conf.py
+++ conf.py
@@ -136,6 +136,8 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
+
+    'releasename': ' ',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
